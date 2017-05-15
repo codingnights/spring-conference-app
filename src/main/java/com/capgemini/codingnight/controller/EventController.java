b6ee0c2101adf6de8ce0a66d3472b81de414b452
Smilde <hiltsje.smilde@capgemini.com>
2017-05-15 20:58:39 +0200

    get and post request event

--- src/main/java/com/capgemini/codingnight/controller/EventController.java
+++ src/main/java/com/capgemini/codingnight/controller/EventController.java
@@ -1,8 +1,39 @@
 package com.capgemini.codingnight.controller;
 
+import com.capgemini.codingnight.model.Event;
+import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by HSMILDE on 15-5-2017.
  */
+@RestController
 public class EventController {
-    
+    private List<Event> eventList = new ArrayList<>();
+
+    @GetMapping("/event")
+    public List<Event> getEvents() {
+        return eventList;
+    }
+    @GetMapping("/event/{id}")
+    public Event getEvent(@PathVariable Long id) {
+        for (Event event : eventList){
+            if (event.getId() == id){
+                return event;
+            }
+        }
+        return null;
+    }
+    @PostMapping("/event")
+    public void postEvent(@RequestBody(required = false) Event event) {
+        if (event != null) {
+            System.out.println("post event");
+            eventList.add(event);
+        } else {
+            System.out.println("post empty event");
+        }
+    }
 }
--- src/main/java/com/capgemini/codingnight/model/Event.java
+++ src/main/java/com/capgemini/codingnight/model/Event.java
@@ -8,12 +8,22 @@ import java.net.URI;
  * Created by HSMILDE on 15-5-2017.
  */
 public class Event {
+    private Long id;
     private String title;
     private EventDate eventDate;
     private String subject;
     private EventLocation eventLocation;
     private URI url;
     private String description;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
